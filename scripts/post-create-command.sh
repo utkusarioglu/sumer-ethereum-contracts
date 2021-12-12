d9edaeb86db642bd9c2264c8641bd2b656725cf7
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-12 16:14:20 +0000

    Add post create command
    - Add post-create-command.sh for installing dependencies and
      simiar actions that need to take place for development to begin.
    - Include the said script in devcontainer.json.

---
+++ scripts/post-create-command.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script file is called by vscode once the devcontainer is ready to
+# run commands. Use this for installing dependencies and similar actions
+# that prepare the container for development
+
+yarn
