39d038b89aec50446a8cf332eb635c2c24e4589b
Utku Sarioglu <utkusarioglu@hotmail.com>
2021-12-18 17:33:28 +0000

    Add mumbai alias in envAccounts service
    - Add mumbai as a network alias, using the same accounts as goerli

--- src/services/env-accounts/env-accounts.service.ts
+++ src/services/env-accounts/env-accounts.service.ts
@@ -4,6 +4,7 @@ const envAccounts = new EnvAccounts()
   .setEnvPrefix("ACCOUNT_")
   .setRequiredAccounts("local_deployer", "local_user1")
   .setNetworkAlias("local", ["hardhat", "geth"])
+  .setNetworkAlias("goerli", ["goerli", "mumbai"])
   .parse();
 
 export default envAccounts;
